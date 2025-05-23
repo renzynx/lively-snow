3bd903aba1a510f95aad397df9e61d988e6afb88
renzynx <69468436+renzynx@users.noreply.github.com>
2025-05-24 03:32:56 +0700

    set host to "0.0.0.0" for server accessibility

--- server/index.ts
+++ server/index.ts
@@ -72,7 +72,7 @@ app.register(remixFastify, {
   },
 });
 
-const host = process.env.HOST === "true" ? "0.0.0.0" : "127.0.0.1";
+const host = "0.0.0.0";
 const desiredPort = Number(process.env.PORT) || 3000;
 getPort({
   port: portNumbers(desiredPort, desiredPort + 100),
